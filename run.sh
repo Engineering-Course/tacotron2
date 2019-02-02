576c1a03cb1f3b052490b1deb1fc617523fb9aee
gongke <gongke@dm-ai.cn>
2019-02-02 21:53:28 +0800

    add bznsyp

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
-python synthesis.py -o resutls/thchs30 -t outdir/thchs30/checkpoint_15990 -w outdir/waveglow_old.pt -l test_list_zh.txt 
+#python synthesis.py -o resutls/thchs30 -t outdir/thchs30/checkpoint_15990 -w outdir/waveglow_old.pt -l test_list_zh.txt 
+CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
