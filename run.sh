4180c04a44780e92fed5a95caf51d13eed4aa682
gongke <gongke@dm-ai.cn>
2019-02-11 14:40:05 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
-python synthesis.py -o resutls -t outdir/tacotron2_statedict.pt -w outdir/waveglow_old.pt -l test_list_en.txt 
+python synthesis.py -o resutls/ljspeech -t outdir/ljspeech/checkpoint_97170 -w outdir/waveglow_old.pt -l test_list_en.txt 
