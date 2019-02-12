4b77467d58cba7cca878ce342272e032ef7305ac
gongke <gongke@dm-ai.cn>
2019-02-12 17:42:05 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
-#python synthesis.py -o resutls/thchs30 -t outdir/thchs30/checkpoint_15990 -w outdir/waveglow_old.pt -l test_list_zh.txt 
-CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
+python synthesis.py -o resutls/bznsyp -t outdir/bznsyp/checkpoint_110000 -w outdir/waveglow_old.pt -l test_list_zh.txt 
+#CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
