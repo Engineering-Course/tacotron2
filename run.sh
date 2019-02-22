1f704100fb0b11d0f8656093ec26d13be9bd8b4f
gongke <gongke@dm-ai.cn>
2019-02-22 12:04:56 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #CUDA_VISIBLE_DEVICES=0 python -m multiproc train.py --output_directory=outdir --log_directory=logdir
-#python synthesis.py -o resutls/ljspeech -t outdir/ljspeech/checkpoint_97170 -w outdir/waveglow_old.pt -l test_list_en.txt 
-python synthesis.py -o resutls/ljspeech -t outdir/ljspeech/checkpoint_97170 -w /home/yy/TTS/waveglow/checkpoints/waveglow_32728 -l test_list_en.txt 
+#python synthesis.py -o resutls/ljspeech -t outdir/ljspeech/checkpoint_97170 -w outdir/waveglow_old.pt -l test_training_list_en.txt 
+python synthesis.py -o resutls/ljspeech -t outdir/ljspeech/checkpoint_97170 -w /home/yy/TTS/waveglow/checkpoints/waveglow_242276 -l test_training_list_en.txt 
